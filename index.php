706f806f4e596654b85bc91237686a7bd8eb361e
ernstt@umsystem.edu <ernstt@umsystem.edu>
2021-06-17 15:32:07 -0500

    Various updates to the parent theme
    Various updates to the header and blog section of the site. Added a featured posts block.

--- index.php
+++ index.php
@@ -22,52 +22,40 @@ get_header(); ?>
 
         
         <div class="row">
-        <div class="col-md-9 main left">
-        <div class="row">
                 
 				<?php if (have_posts()) : ?>
                 <?php while (have_posts()) : the_post(); ?>
             	
-                
-                
-                <?php get_template_part('partials/loop','blog'); ?>
-                
-				
+                <div class="col-md-12">
+					<?php get_template_part('partials/loop','blog'); ?>
+				</div>
+			
                 <?php endwhile; ?>
                 
-                <div class="clear"></div>
-				
-				<?php
-                    $prev_link = get_previous_posts_link(__('Newer Entries &raquo;', 'kubrick'));
-                    $next_link = get_next_posts_link(__('&laquo; Older Entries', 'kubrick'));
+                <?php
+                    $prev_link = get_previous_posts_link(__('&laquo; Newer Entries', 'kubrick'));
+                    $next_link = get_next_posts_link(__('Older Entries &raquo;', 'kubrick'));
                 ?>
                                                 
                 <?php if ($prev_link || $next_link): ?>
                                                 
-                    <nav>
-                        <ul class="pager">
-                            <li><?php echo $next_link; ?></li>
-                            <li><?php echo $prev_link; ?></li>
-                        </ul>
-                    </nav>                    
+                    <div class="col-md-12">
+						<div class="post_nav_wrapper">
+							<?php echo $prev_link; ?>
+							<?php echo $next_link; ?>
+						</div>
+                    </div>                    
 
                 <?php endif; ?>
                 <?php else : ?>
-                    
-                    <h2>Not Found</h2>
-                    <p>Sorry, nothing to show yet.</p>
+                    <div class="col-md-12">
+						<h2>Sorry, nothing has been posted in this category yet.</h2>
+					</div>
                                             
                 <?php endif; ?>
             
-            
-
-            </div><!-- row -->
-            </div><!-- 9 -->
-            <div class="col-md-3 sidebar" role="complementary">
-                <?php get_template_part('partials/sidebar','blog'); ?>
-            </div> 
 
         </div>
-        </div>
-    </div>
+	</div>
+</div>
 <?php get_footer(); ?>
--- partials/loop-blog-card.php
+++ partials/loop-blog-card.php
@@ -1,23 +1,15 @@
-<div class="col-md-6">
-	<a href="<?php the_permalink(); ?>">
-		<div class="card">
-			<div class="card-image">
-			<?php if ( has_post_thumbnail() ) {
-					the_post_thumbnail('blog-card');
-				} else {
-					echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/images/blog-default.jpg" alt="<?php the_title(); ?> Featured Image" />';
-				}
-				?> 
-					<h2 class="cardTitle"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'kubrick'), the_title_attribute('echo=0')); ?>"><?php the_title(); ?></a></h2>
-					<?php echo '<span class="postDate">' . get_the_time(__('F jS, Y', 'kubrick')) . '</span><br/>'; ?>
-				    <?php echo '<span class="postCat">Posted in ' . get_the_category_list(', ') . '</span>'; ?>
-			</div>
 
-			<div class="card-content">
-				<p><?php echo '<p>'. excerpt(70) . '</p>'; ?>
-				<a class="button projectInfo" href="<?php the_permalink(); ?>">Read More</a>
-			</div>
-		                        
-		</div>
-	</a>
-</div>
+<a href="<?php the_permalink(); ?>" class="post_link_wrapper wide">
+	<span class="img_wrapper">
+		<?php if ( has_post_thumbnail() ) { ?>
+			<?php $image_id = get_post_thumbnail_id();
+			$image_url = wp_get_attachment_image_src($image_id,'blog', true); ?>
+			<img src="<?php echo $image_url[0]; ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" />
+		<?php } else { ?>
+			<img src="<?php bloginfo('url'); ?>/wp-content/uploads/2016/03/86480546-700x300.jpg" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" />
+		<?php } ?> 
+	</span>
+	<h3><?php the_title(); ?></h3>
+	<?php echo '<p>'. excerpt(25) . '</p>'; ?>
+	<span class="btn-mayecreate">READ MORE</span>
+</a>
