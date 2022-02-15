36765a7d8fcfa95ee493c71d7b8163a15cbbad1f
ernstt@umsystem.edu <ernstt@umsystem.edu>
2022-02-15 17:26:57 -0600

    Fix for parent theme whitescreening the site

--- includes/mayecreate_post_types.php
+++ includes/mayecreate_post_types.php
@@ -16,8 +16,7 @@ function mayecreate_id_column_content( $column, $id ) {
   }
 }
 
-// WP Menu Categories
-add_action( 'init', 'build_taxonomies', 0 );
+
 
 function mayecreate_create_post_type() {
 
